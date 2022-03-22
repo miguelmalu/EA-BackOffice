0031ce7361b6385b4f485c63bf8f5cc2b18c26e8
Miguel <miguel.mateos@estudiantat.upc.edu>
2022-03-22 21:41:03 +0100

    Navbar integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { ListUsersComponent } from './components/list-users/list-users.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
-    ListUsersComponent
+    ListUsersComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
